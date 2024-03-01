03255df5f13e95303f592e07e7aeeba6c6ceb793
akoofice <akoofice@users.noreply.github.com>
2024-03-01 12:08:23 +0000

    Automated code formatting
    Signed-off-by: akoofice <129665695+akoofice@users.noreply.github.com>

--- app.py
+++ app.py
@@ -2,23 +2,22 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('your mongo client!')
-db = client['loginDB']
-collection = db['users']
+client = MongoClient("your mongo client!")
+db = client["loginDB"]
+collection = db["users"]
 
-@app.route('/submit', methods=['POST'])
+
+@app.route("/submit", methods=["POST"])
 def submit():
     data = request.json
-    first_name = data['firstName']
-    last_name = data['lastName']
+    first_name = data["firstName"]
+    last_name = data["lastName"]
 
-    user_data = {
-        'first_name': first_name,
-        'last_name': last_name
-    }
+    user_data = {"first_name": first_name, "last_name": last_name}
 
     inserted = collection.insert_one(user_data)
-    return jsonify({'message': 'Data inserted successfully'})
+    return jsonify({"message": "Data inserted successfully"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
